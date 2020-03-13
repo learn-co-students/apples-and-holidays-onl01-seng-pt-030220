77359d2644e42e2c00dcc1502521e1c204aae139
Alejandra I. Lee Vargas <leevargasale@gmail.com>
2020-03-13 23:37:30 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,26 +18,61 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  
+  holiday_hash.each do |season, event_hash|
+   
+    if season == :summer
+       event_hash.each do |event, item|
+          return item[1]
+       end
+    end
+  end
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  
+  holiday_hash.each do |season, event_hash|
+    if season == :winter
+      event_hash.each do |event, item|
+        item.push("Balloons")
+      end
+    end
+  end
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  
+  hash = holiday_hash
+  new_item = supply
+  
+    hash.each do |season, event_hash|
+     
+    if season == :spring
+      event_hash.each do |event, item|
+        
+        item.push(supply)
+      end
+    end
+  end
 end
 
-def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
 
+def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
+  
+  new_holiday=holiday_hash[season]
+  new_holiday[holiday_name]
+
+  
+  supply_array.each do |item|
+    holiday_hash[season][holiday_name].push(item)
+  end
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
